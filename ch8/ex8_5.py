2692fd26cd395e5e76d3bdbd5735179251841b7d
Ian Maher <ianpmaher@gmail.com>
2023-03-05 20:30:55 -0500

    caesar cypher - fix

--- ch8/ex8_5.py
+++ ch8/ex8_5.py
@@ -14,8 +14,52 @@ and returns a new string that contains the letters from the original string rota
 You might want to use the built-in function ord, which converts a character to a numeric code, 
 and chr, which converts numeric codes to characters. Letters of the alphabet are encoded in 
 alphabetical order, so 
-> ord('c') - ord('a') = 2
+> ord('c') - ord('a') = 2     #because c is the two-eth letter of the alphabet
 """
+def stringforwards(stringHere):
+    for character_index in stringHere:
+        print(character_index) # print each character one at a time from the string
+
+# I took this directly from Allen Downey's code because I couldn't figure it out
+# "The numeric codes of uppercase letters are different."
+def rotate_letter(letter, n):
+    """Rotates a letter by n places.  Does not change other chars.
+    letter: single-letter string
+    n: int
+    Returns: single-letter string
+    """
+    if letter.isupper():
+        start = ord('A')
+    elif letter.islower():
+        start = ord('a')
+    else:
+        return letter
+
+    c = ord(letter) - start
+    i = (c + n) % 26 + start
+    return chr(i)
+
 def rotate_word(word, numba):
-    numba == int(input())
-    word.
+    # rotates a letter by 'numba' places a la caesar cypher
+    # letter : single letter string
+    rotated_word = ''
+    for letter in word:
+        rotated_word += chr(ord(letter) + numba)
+    return rotated_word
+
+if __name__ == '__main__':
+    print(rotate_word('IBM', -1))
+    print(rotate_word('sleep', 9))
+    print(rotate_word('melon', -10))
+    print(rotate_word('cheer', 7))
+   
+"""
+def prompting():
+    a = input("Type your word to rotate: \n")
+    b = input("Type your number to encrypt the word by: \n")
+    
+    print(rotate_word(str('a'), int(b)))
+
+prompting()
+"""
+# ^ I want to figure out how to make this interactive, it is a cool idea.
