c19203f3d08f2204b970ce3e72924b438872b64d
Ian Maher <ianpmaher@gmail.com>
2023-03-10 08:40:02 -0500

    new file

---
+++ guessnumbergame.py
@@ -0,0 +1,24 @@
+# guess.py
+
+from random import randint
+
+secret = randint(1, 10)
+
+print("Welcome, friend!")
+
+name = ""
+while not name:
+    name = input("Enter your name: ").strip()
+
+while True:
+    user_input = input("Guess a number between 1 and 10: ")
+    if not user_input.isdigit():
+        user_input = input("Please enter a valid number: ")
+    guess = int(user_input)
+    if guess == secret:
+        print(f"Congrats {name}! You win!")
+        break
+    elif guess > secret:
+        print("The secret number is lower than that...")
+    else:
+        print("The secret number is greater than that...")
